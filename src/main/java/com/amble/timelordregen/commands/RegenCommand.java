8ecc7cd5d08f3c3c8c691526a60125dfa9e7214d
James Hall <james.hall01@outlook.com>
2025-09-24 18:02:15 +0100

    feat: add /regen command options for getting and setting regeneration uses

--- src/main/java/com/amble/timelordregen/commands/RegenCommand.java
+++ src/main/java/com/amble/timelordregen/commands/RegenCommand.java
@@ -3,12 +3,14 @@ package com.amble.timelordregen.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.amble.timelordregen.api.RegenerationInfo;
 import com.amble.timelordregen.network.Networking;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 
 // Add this import for literal() if needed:
+import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class RegenCommand {
@@ -17,6 +19,33 @@ public class RegenCommand {
         // Register /regen command
         dispatcher.register(literal(Text.translatable("command.regen.name").getString())
                 .requires(source -> source.hasPermissionLevel(0)) // Allow all players
+				        .then((literal("get").executes(context -> {
+					        var player = context.getSource().getPlayer();
+					        if (player == null) return 0;
+
+					        RegenerationInfo info = RegenerationInfo.get(player);
+					        if (info == null) {
+						        context.getSource().sendError(Text.translatable("command.regen.data.error"));
+						        return 0;
+					        }
+
+							context.getSource().sendFeedback(() -> Text.translatable("gui.regen.settings.remaining", info.getUsesLeft()), false);
+							return 1;
+						})))
+				        .then(literal("set").then(argument("count", IntegerArgumentType.integer(0, RegenerationInfo.MAX_REGENERATIONS)).executes((context) -> {
+					        var player = context.getSource().getPlayer();
+					        if (player == null) return 0;
+
+					        RegenerationInfo info = RegenerationInfo.get(player);
+					        if (info == null) {
+						        context.getSource().sendError(Text.translatable("command.regen.data.error"));
+						        return 0;
+					        }
+
+							info.setUsesLeft(IntegerArgumentType.getInteger(context, "count"));
+					        context.getSource().sendFeedback(() -> Text.translatable("gui.regen.settings.remaining", info.getUsesLeft()), false);
+					        return 1;
+				        })))
                 .executes(context -> {
                     var player = context.getSource().getPlayer();
                     if (player == null) return 0;
